5c3e516d3ed80d0f69cc9ca9d9e4e42b34ce7739
andrioaraujo <afdacampos@inf.ufpel.edu.br>
2015-10-22 18:02:31 -0200

    Create phoenix.sql

---
+++ phoenix.sql
@@ -0,0 +1,55 @@
+-- Deleta a base de dados caso já existir
+DROP DATABASE IF EXISTS BluePhoenix;
+
+-- Cria a base de dados BluePhoenix 
+CREATE DATABASE BluePhoenix;
+
+-- Coloca a base de dados em uso
+USE BluePhoenix;
+
+-- Cria a tabela USUARIO obs: ele reconhece USER como um comando
+CREATE TABLE Usuario(
+	userId 	  	 INT	     		NOT NULL,
+    userEmail	 VARCHAR(45)		NOT NULL,
+    userName  	 VARCHAR(45)		NOT NULL,
+    userPassword VARCHAR(45)		NOT NULL,
+    
+PRIMARY KEY (userId)    
+);
+
+-- Cria a tabela Project
+CREATE TABLE Project(
+	nSeq 								INT,
+    projectMetrics_idProjectMetrics 	INT,
+    classMetrics_idClassMetrics			INT,
+    user_userId							INT,
+    user_email							VARCHAR(45),
+PRIMARY KEY (nSeq),
+FOREIGN KEY (projectMetrics_idProjectMetrics) REFERENCES ProjectMetrics (idProjectMetrics),
+FOREIGN KEY (classMetrics_idClassMetrics) REFERENCES ClassMetrics (idClassMetrics),
+FOREIGN KEY (user_userId) REFERENCES Usuario (userId),
+FOREIGN KEY (user_email) REFERENCES Usuario (userEmail)
+);
+
+-- Cria a tabela ProjectMetrics
+CREATE TABLE ProjectMetrics(
+	idProjectMetrics 	INT,
+    nClass 				INT,
+    nInterfaces			INT,
+    nLines				INT,
+	ditMax				INT,
+PRIMARY KEY (idProjectMetrics)
+);
+
+-- Cria a tabela ClassMetrics
+CREATE TABLE ClassMetrics (
+    idClassMetrics INT,
+    av_sd_nChildren INT,
+    av_sd_nAttributes INT,
+    av_sd_nMethods INT,
+    PRIMARY KEY (idCLassMetrics)
+);
+
+
+
+
